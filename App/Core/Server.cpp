06ae6ae46491d9504e40e5ad84eba26e385de1fc
L <zover.v@gmail.com>
2021-06-14 12:20:57 +0800

    🚧refactor code

--- App/Core/Server.cpp
+++ App/Core/Server.cpp
@@ -1,111 +1,64 @@
 #include "Server.hpp"
 
-#include <cppcoro/async_scope.hpp>
-#include <cppcoro/when_all.hpp>
-
+#include <asio/use_awaitable.hpp>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
-#include <fmt/format.h>
+
+#include <format>
 
 namespace Misaka {
 
-UdpServer::UdpServer(std::string_view address, uint16_t port)
-    : m_Socket(cppcoro::net::socket::create_udpv4(m_IOService)),
-      m_Logger(spdlog::stdout_color_st(
-          fmt::format("UDP Server {}:{}",
-                      address,
-                      port))) {
+std::variant<std::monostate, Request, Response> UdpServer::PaserMessage(
+    uint8_t* data,
+    size_t   size) {
+    std::variant<std::monostate, Request, Response> result;
+    // TODO paser data to request or response
+    return result;
+}
+
+UdpServer::UdpServer(
+    asio::io_context&                              io_context,
+    Kademlia::RouteTable<asio::ip::udp::endpoint>& route_table,
+    std::string_view                               address,
+    uint16_t                                       port)
+    : m_Socket(io_context, asio::ip::udp::endpoint(asio::ip::make_address(address), port)),
+      m_RouteTable(route_table),
+      m_Logger(spdlog::stdout_color_st(std::format("UDP Server {}:{}", address, port))) {
+#if defined(_DEBUG)
+
     m_Logger->set_level(spdlog::level::debug);
 
-    auto _address = cppcoro::net::ipv4_address::from_string(address);
-    if (_address) {
-        m_Endpoint = cppcoro::net::ipv4_endpoint(*_address, port);
-    } else {
-        m_Logger->error("error format of address: {}", address);
-        m_Logger->warn("will use address 0.0.0.0 as address");
-        m_Endpoint = cppcoro::net::ipv4_endpoint(cppcoro::net::ipv4_address(), port);
-    }
-
-    m_Socket.bind(m_Endpoint);
-    m_Logger->info("Host:{}", m_Endpoint.to_string());
+#endif  // _DEBUG
 }
 
-cppcoro::task<void> UdpServer::Listen() {
+asio::awaitable<void> UdpServer::Listen() {
     m_Logger->info("start listening.");
-    std::exception_ptr ex;
-
     try {
+        asio::ip::udp::endpoint remote;
         while (true) {
-            auto [len, remote] = co_await m_Socket.recv_from(m_Buffer.data(), m_Buffer.size(), m_Canceller.token());
-            m_Logger->debug(
-                "Get data from: {}"
-                "Data: {}",
-                remote.to_string(), std::string_view(m_Buffer.data(), len));
+            size_t n = co_await m_Socket.async_receive_from(
+                asio::buffer(m_ReciveBuffer.data(), m_ReciveBuffer.size()),
+                remote,
+                asio::use_awaitable);
+
+            auto data = PaserMessage(m_ReciveBuffer.data(), n);
+            if (std::holds_alternative<Request>(data))
+                ProcessRequest(std::get<Request>(data));
+            else if (std::holds_alternative<Response>(data))
+                ProcessResponse(std::get<Response>(data));
+            else
+                m_Logger->warn(
+                    "discard the data from: {}:{}",
+                    remote.address().to_string(),
+                    remote.port());
         }
-    } catch (cppcoro::operation_cancelled) {
-        m_Logger->info("stop listening.");
-    } catch (...) {
-        ex = std::current_exception();
-    }
-
-    if (ex) std::rethrow_exception(ex);
-}
-
-void UdpServer::Stop() {
-    m_Canceller.request_cancellation();
-}
-
-cppcoro::task<NodeState> UdpServer::Ping(const cppcoro::net::ip_endpoint& remote) {
-    auto message = GenerateMessage();
-    auto buffer  = message.SerializeAsString();
-
-    m_Logger->debug("remote: {}", remote.address().to_string());
-
-    co_await SendMessage(remote, message);
-
-    // wait response here
-    co_return NodeState::RUNNING;
-}
-
-cppcoro::task<Response> UdpServer::SendRequest(Request request, const cppcoro::net::ip_endpoint& remote) {
-    auto message = GenerateMessage();
-    message.set_allocated_request(&request);
-
-    co_await SendMessage(remote, message);
-    // wait response here
-
-    co_return Response{};
-}
-
-cppcoro::task<void> UdpServer::SendMessage(const cppcoro::net::ip_endpoint& remote, const KademliaMessage& message) {
-    auto   buffer      = message.SerializeAsString();
-    size_t buffer_size = buffer.size();
-    m_Logger->debug("send message, {} bytes(s)", buffer.size());
-
-    try {
-        size_t bytes_send = 0;
-        while (bytes_send < buffer_size) {
-            bytes_send += co_await m_Socket.send_to(remote, buffer.data() + bytes_send, buffer_size - bytes_send);
-        }
-
-        m_Socket.close_send();
-
-    } catch (const std::exception& e) {
+    } catch (std::exception e) {
         m_Logger->error(e.what());
     }
 }
 
-KademliaMessage UdpServer::GenerateMessage() {
-    KademliaMessage message;
-    message.set_messageid(GenerateMessageID());
-    message.set_state(NodeState::RUNNING);
-    return message;
-}
-
-uint64_t UdpServer::GenerateMessageID() {
-    // TODO
-    thread_local uint64_t counter = 1;
-    return counter++;
+asio::awaitable<Response> UdpServer::Send(Request request) {
+    // m_RouteTable.Get()
 }
 
 }  // namespace Misaka
--- App/Core/Server.hpp
+++ App/Core/Server.hpp
@@ -1,36 +1,49 @@
 #pragma once
 #include "kademlia.pb.h"
+#include "RouteTable.hpp"
 
 #include <asio/awaitable.hpp>
 #include <asio/ip/udp.hpp>
 #include <spdlog/logger.h>
 
 #include <array>
-#include <optional>
-#include <chrono>
+#include <variant>
 
 namespace Misaka {
 
 class UdpServer {
 public:
-    UdpServer(std::string_view address, uint16_t port);
+    UdpServer(
+        asio::io_context&                              io_context,
+        Kademlia::RouteTable<asio::ip::udp::endpoint>& route_table,
+        std::string_view                               address = "127.0.0.1",
+        uint16_t                                       port    = 8000);
+
     // Bind endpoint to udp server
     void Bind(std::string_view address, uint16_t port);
-    // Set a request timeout
-    void SetTimeout(std::chrono::duration timeout);
+    // Set request processor
+    inline void SetRequestProcessor(std::function<void(const Request&)> processor) noexcept {
+        m_RequestProcessor = processor;
+    }
 
     asio::awaitable<void>     Listen();
-    asio::awaitable<Response> SendRequest(Request request);
+    asio::awaitable<Response> Send(Request request);
 
 private:
-    void                ParseMessage(std::string_view message, const asio::ip::udp::endpoint& endpoint);
-    KademliaMessage     GenerateMessage();
-    cppcoro::task<void> SendMessage(const cppcoro::net::ip_endpoint& remote, const KademliaMessage& message);
-    uint64_t            GenerateMessageID();
+    static std::variant<std::monostate, Request, Response> PaserMessage(uint8_t* data, size_t size);
+
+    void ProcessRequest(const Request& request);
+    void ProcessResponse(const Response& response);
+
+    // asio
+    asio::ip::udp::socket     m_Socket;
+    std::array<uint8_t, 8192> m_ReciveBuffer;
+
+    // route table
+    Kademlia::RouteTable<asio::ip::udp::endpoint>& m_RouteTable;
 
     std::function<void(const Request&)> m_RequestProcessor;
-
-    std::shared_ptr<spdlog::logger> m_Logger;
+    std::shared_ptr<spdlog::logger>     m_Logger;
 };
 
 }  // namespace Misaka
